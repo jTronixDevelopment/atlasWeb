f2cbef5d8ad306d52dc46b0d44055f0dae166fdd
Ryan Johnson <johnson2500@live.com>
2018-04-13 23:10:34 -0700

    Out with the old in with the new

--- src/firebase/database/database.js
+++
@@ -1,9 +0,0 @@
-export default class DB{
-  constructor(firebase){
-    this.firebase = firebase;
-  }
-
-  write(DBname, data){
-    this.firebase.database().ref('users/' + data.userId).set(data);
-  }
-}
