cb4608b3549e05e0ca1bab44afd7376239b77c10
Ryan Johnson <johnson2500@live.com>
2018-04-15 22:57:40 -0700

    Removing Index
    Removing index and adding app.css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import AppReducers from './redux/reducermain.js';
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 let store = createStore(AppReducers)
 
