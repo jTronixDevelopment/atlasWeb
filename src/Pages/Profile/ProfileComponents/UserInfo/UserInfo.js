ddbe8838956ccd80fb6970b088893a31dba184fa
Ryan Johnson <johnson2500@live.com>
2018-05-15 14:03:01 -0700

    Stying CSS for user info

--- src/Pages/Profile/ProfileComponents/UserInfo/UserInfo.js
+++ src/Pages/Profile/ProfileComponents/UserInfo/UserInfo.js
@@ -7,15 +7,17 @@ import './userinfo.css'
 export default class App extends Component {
   render() {
     return (
-        <Jumbotron fluid className='profile-jumbotron text-center'>
-          <img alt='profileIcon' className='profile-pic' src= { this.props.profileData.profilePic } />
-          <div className='profile-bio'>
-            <b>Bio</b>
-            <p>{ `${this.props.profileData.firstName} ${this.props.profileData.lastName}` }</p>
-            <b>About me</b>
-            <p>{ this.props.profileData.bio }</p>
-            <b>Hometown</b>
-            <p>{ this.props.profileData.homeTown }</p>
+        <Jumbotron fluid className='profile-jumbotron'>
+          <div className='flex-container'>
+            <img alt='profileIcon' className='profile-pic' src= { this.props.profileData.profilePic } />
+            <div className='profile-bio'>
+              <b>Bio</b>
+              <p>{ `${this.props.profileData.firstName} ${this.props.profileData.lastName}` }</p>
+              <b>About me</b>
+              <p>{ this.props.profileData.bio }</p>
+              <b>Hometown</b>
+              <p>{ this.props.profileData.homeTown }</p>
+            </div>
           </div>
         </Jumbotron>
     );
